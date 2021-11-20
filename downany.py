c1d6af3e210bce954316fc6d6c3261988976335a
Ganer <34655998+GanerCodes@users.noreply.github.com>
2021-11-20 02:42:23 -0600

    sdfg

--- downany.py
+++ downany.py
@@ -6,24 +6,24 @@ qsha = lambda data: sha256(data.encode()).hexdigest()
 
 collapseList = lambda l, n = None: (collapseList(l, j := []) and j) if n is None else ([collapseList(i, n) for i in l] if isinstance(l, list) else n.append(l))
 
-def downloadYoutube(link, noPlaylist = False, audioOnly = False, folder = ""):
+def downloadYoutube(link, noPlaylist = False, audioOnly = False, folder = "", cookies = "cookies.txt"):
     return (folder, Popen(collapseList([
         "yt-dlp", ['-x', "--audio-format", "mp3"] if audioOnly else ["--merge-output-format", "mp4"], "--no-progress", "--embed-thumbnail",
-        "--no-post-overwrites", "-ciw", ["--no-playlist"] if noPlaylist else [], "--cookies", "cookies.txt", "--restrict-filenames",
+        "--no-post-overwrites", "-ciw", ["--no-playlist"] if noPlaylist else [], "--cookies", cookies, "--restrict-filenames",
         "-o", f"{folder}/%(playlist_index)s_%(title)s_%(id)s.%(ext)s", link
     ])))
 
-def downloadGallery(link, folder = ""):
+def downloadGallery(link, folder = "", cookies = "cookies.txt"):
     return (folder, Popen(collapseList([
-        "gallery-dl", "--config", "gallery_dl.json", "-o", f"base-directory={folder}", link
+        "gallery-dl", "--config", "gallery_dl.json", "--cookies", cookies, "-o", f"base-directory={folder}", link
     ])))
 
-def downloadWget(link, folder = ""):
+def downloadWget(link, folder = "", cookies =  "cookies.txt"):
     return (folder, Popen(collapseList([
-        "wget", "--load-cookies", "cookies.txt", "-c", "-nd", "-nv", "-P", f"{folder}", link
+        "wget", "--load-cookies", cookies, "-c", "-nd", "-nv", "-P", f"{folder}", link
     ])))
 
-def download(link, args = "", baseDir = "download"):
+def download(link, args = "", baseDir = "download", cookies = "cookies.txt"):
     args = list(filter(None, args.lower().split(' ')))
     baseFold = f"{baseDir}/{(hsh := qsha(link)[:24])}"
     
@@ -34,16 +34,16 @@ def download(link, args = "", baseDir = "download"):
         args = "video gallery wget"
     if "video" in args:
         print("Downloading as: video")
-        procs.append(("video", downloadYoutube(link, audioOnly = 0, noPlaylist = ("noplaylist" in args), folder = f"{baseFold}/video")))
+        procs.append(("video", downloadYoutube(link, audioOnly = 0, noPlaylist = ("noplaylist" in args), folder = f"{baseFold}/video", cookies = cookies)))
     if "audio" in args:
         print("Downloading as: audio")
-        procs.append(("audio", downloadYoutube(link, audioOnly = 1, noPlaylist = ("noplaylist" in args), folder = f"{baseFold}/audio")))
+        procs.append(("audio", downloadYoutube(link, audioOnly = 1, noPlaylist = ("noplaylist" in args), folder = f"{baseFold}/audio", cookies = cookies)))
     if "gallery" in args or "image" in args:
         print("Downloading as: gallery")
-        procs.append(("gallery", downloadGallery(link, folder = f"{baseFold}/gallery")))
+        procs.append(("gallery", downloadGallery(link, folder = f"{baseFold}/gallery", cookies = cookies)))
     if "wget" in args or "file" in args:
         print("Downloading as: wget")
-        procs.append(("wget", downloadWget(link, folder = f"{baseFold}/wget")))
+        procs.append(("wget", downloadWget(link, folder = f"{baseFold}/wget", cookies = cookies)))
     
     final = []
     for proc in procs:
